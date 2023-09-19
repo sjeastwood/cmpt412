3d1db7d78229b6a560a774d871dae0144235d012
Shane Eastwood <seastwoo@sfu.ca>
2023-09-18 17:46:38 -0700

    test the push

---
+++ project1/test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[2]:
+
+
+import os
+
+
+# In[7]:
+
+
+test = np.zeros(5)
+
+
+# In[8]:
+
+
+test
+
+
+# In[ ]:
+
+
+
+
