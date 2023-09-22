679ea9e98359a2aeb1975a5c3064cba09a63fd0a
Shane Eastwood <seastwoo@sfu.ca>
2023-09-22 14:28:01 -0700

    generated confusion matrix

--- project1/python/test_network.py
+++ project1/python/test_network.py
@@ -5,7 +5,7 @@ from scipy.io import loadmat
 from conv_net import convnet_forward
 from init_convnet import init_convnet
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 # Load the model architecture
 layers = get_lenet()
@@ -31,42 +31,35 @@ xtrain, ytrain, xvalidate, yvalidate, xtest, ytest = load_mnist(fullset)
 # Testing the network
 #### Modify the code to get the confusion matrix ####
 all_preds = []
+ground_truth = []
 for i in range(0, xtest.shape[1], 100):
     cptest, P = convnet_forward(params, layers, xtest[:,i:i+100], test=True)
     all_preds.append(P)
-
-# hint: 
-#     you can use confusion_matrix from sklearn.metrics (pip install -U scikit-learn)
-#     to compute the confusion matrix. Or you can write your own code :)
-
-# print(confusion_matrix(ytest, P))
-
-
-############ FROM chatGPT ################
-# from sklearn.metrics import confusion_matrix
-
-# # Initialize an empty list to store all the predictions and true labels
-# all_preds = []
-# true_labels = []
-
-# # Loop through the test data in batches of 100 as you did before
-# for i in range(0, xtest.shape[1], 100):
-#     cptest, P = convnet_forward(params, layers, xtest[:, i:i+100], test=True)
+    ground_truth.extend(ytest[:, i:i+100])
     
-#     # Append the batch predictions to the list of predictions
-#     all_preds.append(P)
-    
-#     # Append the true labels of this batch to the list of true labels
-#     true_labels.extend(ytest[:, i:i+100])
+# Combine all the predictions into a single numpy array
+y_pred = np.concatenate(all_preds, axis=1)
 
-# # Combine all the predictions into a single numpy array
-# y_pred = np.concatenate(all_preds, axis=1)
+y_pred_val = []
 
-# # Flatten the list of true labels into a 1D numpy array
-# y_true = np.array(true_labels).flatten()
+# now go through all the y_preds and get the index representing the predicted digit and store it
+for i in range(y_pred.shape[1]):
+    y_pred_val.append(np.argmax(y_pred[:,i]))
 
-# # Calculate the confusion matrix
-# confusion = confusion_matrix(y_true, np.argmax(y_pred, axis=0))
+y_pred_flatten = np.array(y_pred_val).flatten() #make it a 1d array
 
-# # Print or use the confusion matrix as needed
-# print(confusion)
+y_true = np.array(ground_truth).flatten() #make it a 1d array
+
+labels=[0,1,2,3,4,5,6,7,8,9]
+
+cm = confusion_matrix(y_true, y_pred_flatten, labels=labels)
+
+cm_disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
+cm_disp.plot()
+
+#convert to matplotlib figure and save it to results
+cm_disp.figure_.suptitle('Confusion Matrix')
+
+cm_disp.figure_.savefig('../results/confusion_matrix.png',dpi=300)
+
+
