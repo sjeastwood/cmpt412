5dcc25e3be8f70b8f114909b9ba01cdda26cf715
shane <sjeastwood72@gmail.com>
2023-09-22 08:44:21 -0700

    working on testing network and confusion matrix

--- project1/python/test_network.py
+++ project1/python/test_network.py
@@ -5,6 +5,7 @@ from scipy.io import loadmat
 from conv_net import convnet_forward
 from init_convnet import init_convnet
 import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
 
 # Load the model architecture
 layers = get_lenet()
@@ -32,8 +33,40 @@ xtrain, ytrain, xvalidate, yvalidate, xtest, ytest = load_mnist(fullset)
 all_preds = []
 for i in range(0, xtest.shape[1], 100):
     cptest, P = convnet_forward(params, layers, xtest[:,i:i+100], test=True)
+    all_preds.append(P)
 
 # hint: 
 #     you can use confusion_matrix from sklearn.metrics (pip install -U scikit-learn)
 #     to compute the confusion matrix. Or you can write your own code :)
 
+# print(confusion_matrix(ytest, P))
+
+
+############ FROM chatGPT ################
+# from sklearn.metrics import confusion_matrix
+
+# # Initialize an empty list to store all the predictions and true labels
+# all_preds = []
+# true_labels = []
+
+# # Loop through the test data in batches of 100 as you did before
+# for i in range(0, xtest.shape[1], 100):
+#     cptest, P = convnet_forward(params, layers, xtest[:, i:i+100], test=True)
+    
+#     # Append the batch predictions to the list of predictions
+#     all_preds.append(P)
+    
+#     # Append the true labels of this batch to the list of true labels
+#     true_labels.extend(ytest[:, i:i+100])
+
+# # Combine all the predictions into a single numpy array
+# y_pred = np.concatenate(all_preds, axis=1)
+
+# # Flatten the list of true labels into a 1D numpy array
+# y_true = np.array(true_labels).flatten()
+
+# # Calculate the confusion matrix
+# confusion = confusion_matrix(y_true, np.argmax(y_pred, axis=0))
+
+# # Print or use the confusion matrix as needed
+# print(confusion)
