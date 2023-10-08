c37488fd123050fd9dca77c8755c8a0d2af95ab5
Shane Eastwood <35248293+sjeastwood@users.noreply.github.com>
2023-10-08 01:17:54 -0700

    adding new trials

---
+++ project2/trials/trials.sh
@@ -0,0 +1,15 @@
+#1/bin/bash
+python3 train_lr03_gamma1.py >> train_lr03_gamma1.txt
+python3 train_lr05_gamma1.py >> train_lr05_gamma1.txt
+python3 train_lr07_gamma1.py >> train_lr07_gamma1.txt
+python3 train_lr03_gamma5.py >> train_lr03_gamma5.txt
+python3 train_lr05_gamma5.py >> train_lr05_gamma5.txt
+python3 train_lr07_gamma5.py >> train_lr07_gamma5.txt
+python3 train_lr03_gamma9.py >> train_lr03_gamma9.txt
+python3 train_lr05_gamma9.py >> train_lr05_gamma9.txt
+python3 train_lr07_gamma9.py >> train_lr07_gamma9.txt
+python3 train_lr01_gamma9.py >> train_lr01_gamma9.txt
+python3 train_lr01_gamma1.py >> train_lr01_gamma1.txt
+python3 train_lr001_gamma9.py >> train_lr001_gamma9.txt
+python3 train_lr001_gamma1.py >> train_lr001_gamma1.txt
+python3 train_lr001_gamma5.py >> train_lr001_gamma5.txt
